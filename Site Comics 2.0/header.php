2f4616166e5cb7c11082a50c70ccf1e9578b4c06
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-06-07 01:13:54 -0300

    Add files via upload

---
+++ Site Comics 2.0/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header> 
+        <div class="Login">
+        <a href="login.php">Login</a>
+        </div>
+
+
+        <div class="Icones"> 
+            <table style="width:100% ">
+            
+            
+                <tr>
+
+            
+                <a href="index.php">Home</a>
+                <a href="comics.php">Comics</a>
+                <a href="mangas.php">Mangas</a> 
+                <a href="carrinho.php">Meu Carinho</a>
+                <a href="dados.php">Dados</a>
+                
+
+        
+                </tr>
+        
+
+    
+            </table>
+    
+        </div>
+
+    </header>
+</body>
+</html>
